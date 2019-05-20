2ec704de8d4d8fcc8ad1483a0cb08f6f4efaddf2
Ant0wan <abarthel@student.42.fr>
2019-05-20 18:32:27 +0200

    go try

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/14 17:18:53 by abarthel          #+#    #+#             */
-/*   Updated: 2019/05/20 18:02:16 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/05/20 18:32:20 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 20000
+# define BUFF_SIZE 1024
 
 int	get_next_line(const int fd, char **line);
 
